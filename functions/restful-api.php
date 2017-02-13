a9e4eed9f139e05adf5744ccd815d21ab34e5a33
JPrageeth <infopoclion@gmail.com>
2017-02-13 11:49:39 +0900

    Create restful-api.php

---
+++ functions/restful-api.php
@@ -0,0 +1,46 @@
+<?php
+/* Prageeth Sudarshana | Tokyo */
+
+/*
+   Restful Api
+*/
+
+function restful_api($method, $url, $data = false, $passwd = '') {
+
+	$curl = curl_init();
+
+	switch ($method) {
+		case "POST":
+			curl_setopt($curl, CURLOPT_POST, 1);
+
+			if ($data)
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+			break;
+		case "PUT":
+			curl_setopt($curl, CURLOPT_PUT, 1);
+			break;
+		default:
+			if ($data)
+				$url = sprintf("%s?%s", $url, http_build_query($data));
+	}
+	
+
+	// Optional Authentication:
+	if (!empty($passwd)) {
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "{$passwd}");
+	}
+
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+	$result = curl_exec($curl);
+
+	curl_close($curl);
+
+ 
+	return $result;
+}
+
+
+?>
