9666054e6c38e29fdca010dd3c005719992f99c6
JPrageeth <infopoclion@gmail.com>
2017-02-13 14:17:45 +0900

    Create create-http-query.php

---
+++ functions/create-http-query.php
@@ -0,0 +1,27 @@
+	/*
+		ARRAY TO HTTP QUERY SETTING
+    
+	*/
+	function HTTP_QUERY($array, $unset) {
+		if ($array['page']) {
+			unset($array['page']);
+		}
+		
+		if (is_array($array)) {
+			if (is_array($unset)) {
+				foreach ($unset as $key => $val) {
+					unset($array[$unset[$key]]);
+					
+				} 
+				
+			} else {
+				unset($array[$unset]);			
+			}
+	
+			return http_build_query($array) . "\n";
+		
+		} else {
+			return http_build_query($array) . "\n";	
+		}
+		
+	}
