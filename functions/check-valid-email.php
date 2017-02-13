61ebdf8be61edce5aab1e51d854d5e73e63c84c0
JPrageeth <infopoclion@gmail.com>
2017-02-13 17:05:02 +0900

    Update check-valid-email.php

--- functions/check-valid-email.php
+++ functions/check-valid-email.php
@@ -2,28 +2,29 @@
   /* Prageeth Sudarshana | Tokyo */
   
   /*
-		mail check
-	*/
-	
-	function validate_email($email) {
+	mail check
+  */
 
-   		$exp = "/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/";
+function validate_email($email) {
 
-	   if (preg_match($exp,$email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	
-		  if (checkdnsrr(array_pop(explode("@",$email)),"MX")){
-			return true;
-		  
-		  } else {
-			return false;
-		  }
-	
-	   } else {
-	
-		  return false;
-	
-	   }    
-	}
+$exp = "/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/";
+
+   if (preg_match($exp,$email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+	  if (checkdnsrr(array_pop(explode("@",$email)),"MX")){
+		return true;
+
+	  } else {
+		return false;
+	  }
+
+   } else {
+
+	  return false;
+
+   }    
+}
+
 
 
 
