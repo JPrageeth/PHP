a8a5fde940e56fff1fb7e58a4939361ed3f68c1d
JPrageeth <infopoclion@gmail.com>
2017-02-13 14:30:56 +0900

    Update image-resize.php

--- functions/image-resize.php
+++ functions/image-resize.php
@@ -1,31 +1,31 @@
 <?php
-  /* Prageeth Sudarshana | Tokyo */
+/* Prageeth Sudarshana | Tokyo */
 
-  /*
-		Images Setting
-	*/
-	
-	function IMGRESIZE($image, $maxwidth, $maxheight) {
-		
-		list($width,$height) = getimagesize($image);
-	
-		if ($width > $maxwidth) {
-		
-			$newheight = $maxwidth/$width * $height;
-		
-			if($newheight > $maxheight){
-				$maxwidth = ($maxheight * $maxwidth)/$newheight;
-				$newheight = $maxheight;
-			}
-		
-			return " width=\"{$maxwidth}\" height=\"{$newheight}\"";
-		
-		} else {
-	
-			return " width=\"{$maxwidth}\" height=\"{$newheight}\"";
-	
-		}		
-					
-	}
+/*
+	Resize Image Width and Height
+*/
+
+function IMGRESIZE($image, $maxwidth, $maxheight) {
+
+	list($width,$height) = getimagesize($image);
+
+	if ($width > $maxwidth) {
+
+		$newheight = $maxwidth/$width * $height;
+
+		if($newheight > $maxheight){
+			$maxwidth = ($maxheight * $maxwidth)/$newheight;
+			$newheight = $maxheight;
+		}
+
+		return " width=\"{$maxwidth}\" height=\"{$newheight}\"";
+
+	} else {
+
+		return " width=\"{$maxwidth}\" height=\"{$newheight}\"";
+
+	}		
+
+}
 
 ?>
