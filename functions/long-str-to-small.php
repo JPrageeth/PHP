6abed2d51521c0aa663cb921bb14f2437124bf25
JPrageeth <infopoclion@gmail.com>
2017-02-13 14:22:23 +0900

    Update long-str-to-small.php

--- functions/long-str-to-small.php
+++ functions/long-str-to-small.php
@@ -1,23 +1,25 @@
 <?php
-	/*
-		Sub str setting
-	*/
-	
-	function SUBTXT($txt, $size) {
-		mb_internal_encoding("UTF-8");
-		
-		
-		$txt = str_replace(array("\r\n", "\n", "\r"),'', $txt);
-		
-		$var['size'] = mb_strlen($txt);
-		
-		if ($var['size'] > $size) {
-			return mb_substr($txt, 0, $size).'...';
-			
-		} else {
-			return $txt;	
-		}
-	}	
+
+/* Prageeth Sudarshana | Tokyo */
+/*
+	Sub str setting
+*/
+
+function SUBTXT($txt, $size) {
+	mb_internal_encoding("UTF-8");
+
+
+	$txt = str_replace(array("\r\n", "\n", "\r"),'', $txt);
+
+	$var['size'] = mb_strlen($txt);
+
+	if ($var['size'] > $size) {
+		return mb_substr($txt, 0, $size).'...';
+
+	} else {
+		return $txt;	
+	}
+}	
 
 
 ?>
